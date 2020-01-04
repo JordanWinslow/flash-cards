1b174642f3da307a52280eeee0ad58af730f0a7f
JordanWinslow <admin@jordanwinslow.me>
2020-01-04 14:06:13 -0800

    removed redundant styling

--- src/features/flashcard/CreateFlashCard.tsx
+++ src/features/flashcard/CreateFlashCard.tsx
@@ -46,9 +46,7 @@ const useStyles = makeStyles({
     Overrides are not nearly as trivial as they should be and by the time you master
     their naming conventions you could have already made your own design system!
     */
-    "& .MuiPaper-elevation1": {
-      boxShadow: "0 4px 20px rgba(0,0,0, 0.5)"
-    }, //HEADER CONTENT OF FLASHCARD
+    //HEADER CONTENT OF FLASHCARD
     "& .MuiCardHeader-root": {
       background:
         "radial-gradient(circle at 17% 1%, rgba(198, 198, 198,0.03) 0%, rgba(198, 198, 198,0.03) 50%,rgba(42, 42, 42,0.03) 50%, rgba(42, 42, 42,0.03) 100%),radial-gradient(circle at 8% 81%, rgba(253, 253, 253,0.03) 0%, rgba(253, 253, 253,0.03) 50%,rgba(36, 36, 36,0.03) 50%, rgba(36, 36, 36,0.03) 100%),radial-gradient(circle at 83% 29%, rgba(164, 164, 164,0.03) 0%, rgba(164, 164, 164,0.03) 50%,rgba(60, 60, 60,0.03) 50%, rgba(60, 60, 60,0.03) 100%),radial-gradient(circle at 96% 62%, rgba(170, 170, 170,0.03) 0%, rgba(170, 170, 170,0.03) 50%,rgba(169, 169, 169,0.03) 50%, rgba(169, 169, 169,0.03) 100%),linear-gradient(338deg, rgb(2, 141, 213),rgb(5, 172, 81))"
@@ -57,7 +55,6 @@ const useStyles = makeStyles({
       background:
         "linear-gradient(45deg, rgba(86, 86, 86,0.04) 0%, rgba(86, 86, 86,0.04) 50%,rgba(169, 169, 169,0.04) 50%, rgba(169, 169, 169,0.04) 71%,rgba(251, 251, 251,0.04) 71%, rgba(251, 251, 251,0.04) 100%), linear-gradient(45deg, rgba(86, 86, 86,0.04) 0%, rgba(86, 86, 86,0.04) 56%,rgba(169, 169, 169,0.04) 56%, rgba(169, 169, 169,0.04) 67%,rgba(251, 251, 251,0.04) 67%, rgba(251, 251, 251,0.04) 100%), linear-gradient(135deg, rgba(86, 86, 86,0.04) 0%, rgba(86, 86, 86,0.04) 4%,rgba(169, 169, 169,0.04) 4%, rgba(169, 169, 169,0.04) 75%,rgba(251, 251, 251,0.04) 75%, rgba(251, 251, 251,0.04) 100%), linear-gradient(90deg, rgb(0,0,0),rgb(0,0,0))",
       color: "#EEEEEE",
-      boxShadow: "0 4px 20px rgba(0,0,0, 0.5)",
       minHeight: "25vh"
     }, //NORMAL BORDER COLOR
     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
