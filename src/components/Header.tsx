ce093be0a6302e6be527bc7f6acdd92fd1f0034e
JordanWinslow <admin@jordanwinslow.me>
2020-01-04 20:12:15 -0800

    added tooltips on hover & css variables for color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import styled from "styled-components"
 
-import styled from "@material-ui/core/styles/styled"
+import materialStyled from "@material-ui/core/styles/styled"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import AppBar from "@material-ui/core/AppBar"
@@ -10,10 +10,43 @@ import GitHubIcon from "@material-ui/icons/GitHub"
 
 import JWLogo from "../images/JWLogo.svg"
 
-const HeaderContainer = styled(AppBar)({
+const HeaderContainer = materialStyled(AppBar)({
   background:
     "radial-gradient(circle at 16% 83%, rgba(148, 148, 148,0.06) 0%, rgba(148, 148, 148,0.06) 50%,rgba(63, 63, 63,0.06) 50%, rgba(63, 63, 63,0.06) 100%),radial-gradient(circle at 68% 87%, rgba(66, 66, 66,0.06) 0%, rgba(66, 66, 66,0.06) 50%,rgba(105, 105, 105,0.06) 50%, rgba(105, 105, 105,0.06) 100%),radial-gradient(circle at 38% 50%, rgba(123, 123, 123,0.06) 0%, rgba(123, 123, 123,0.06) 50%,rgba(172, 172, 172,0.06) 50%, rgba(172, 172, 172,0.06) 100%),linear-gradient(90deg, hsl(18,0%,1%),hsl(18,0%,1%))"
 })
+
+const ToolTip: any = styled.span`
+  position: absolute;
+  right: ${(props: any) =>
+    props.right
+      ? "2rem"
+      : ""}; // conditionally locate the tooltip to the right or left of the hover element
+  pointer-events: none;
+  opacity: 0;
+  width: auto;
+  background-color: var(--black);
+  color: var(--white);
+  text-align: center;
+  border-radius: 6px;
+  padding: 7px 20px;
+  font-size: 1rem;
+`
+
+const DetectHover = styled.div`
+  transition-duration: 0.3s;
+  /*MAKE TOOLTIP VISIBLE ON HOVER*/
+  span {
+    transition: opacity 0.3s ease-in;
+    transition-delay: 1s;
+  }
+  :hover {
+    color: #fcb813;
+    span {
+      /******TOOLTIP******/
+      opacity: 1;
+    }
+  }
+`
 
 const Header = () => {
   return (
@@ -24,11 +57,14 @@ const Header = () => {
           style={{ position: "relative", left: "-.5rem" }}
         >
           <IconButton>
-            <img
-              src={JWLogo}
-              alt="Logo Link to Jordan Winslow's Portfolio"
-              style={{ width: "3.5rem" }}
-            />
+            <DetectHover>
+              <img
+                src={JWLogo}
+                alt="Logo Link to Jordan Winslow's Portfolio"
+                style={{ width: "3.5rem" }}
+              />
+              <ToolTip>Check Out My Portfolio!</ToolTip>
+            </DetectHover>
           </IconButton>
         </a>
         <Typography variant="body2" component="h1">
@@ -44,7 +80,10 @@ const Header = () => {
           }}
         >
           <IconButton style={{ color: "white" }}>
-            <GitHubIcon fontSize="large" />
+            <DetectHover>
+              <ToolTip right>View Source Code on GitHub</ToolTip>
+              <GitHubIcon fontSize="large" />
+            </DetectHover>
           </IconButton>
         </a>
       </Toolbar>
